b74145d3311777d6f4cc166679068ee57950e520
Sara <134113685+saravorhaben@users.noreply.github.com>
2026-02-23 12:08:56 -0600

    removed extra style sheet

--- src/pages/ParentPortal.js
+++ src/pages/ParentPortal.js
@@ -1,5 +1,35 @@
+import "../App.css"
+import "./parentPortal.css"
 export default function ParentPortal(){
-    return(
-        <h1>This is the Parent Portal</h1>
+    return(<div className="parent-portal">
+        <h1>Parent Name's Students:</h1>
+        <div className= "wholeScreen">
+            <div className = "mainSection">
+                {/* Table of children */}
+                <table className="parent-table">
+                    <tr>
+                        <th className="parent-table">Student Name</th>
+                        <th>Pickup Status</th>
+                        <th>Approved Licence Plate</th>
+                    </tr>
+                    <tr>
+                        <td>Student </td>
+                        <td>Pickup Status</td>
+                        <td>Approved LP</td>
+                    </tr>
+                </table>
+
+                {/* Update Buttons */}
+                <div className = "btn-row">
+                    <button className="main-btn">Update License Plate</button>
+                    <button className="main-btn">Add a New Child</button>
+                </div>
+            </div>
+            <div className="sidebar">
+                
+            </div>
+        </div>
+        
+    </div>
     );
 }
