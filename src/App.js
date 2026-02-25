5d2e3320ce9f9173239cb88b6dba820a7b68aa0e
Sara <134113685+saravorhaben@users.noreply.github.com>
2026-02-25 11:41:07 -0600

    removed unnecseary query

--- src/App.js
+++ src/App.js
@@ -6,27 +6,11 @@ import StaffPortal from "./pages/StaffPortal";
 import KidsPickup from "./pages/KidsPickup";
 import Home from "./pages/Home"
 import CreateAccount from "./pages/CreateAccount";
-import { useEffect} from "react";
-import { supabase } from "./supabaseClient";
+
 
 function App() {
   // Database check
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await supabase
-        .from("students")
-        .select("*");
-
-      if (error) {
-        console.error("Error:", error);
-      } else {
-        console.log("Data:", data);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+  
   
   return ( <>
       {/* Moved login page code to Home.js so that Routes would work correctly */}
