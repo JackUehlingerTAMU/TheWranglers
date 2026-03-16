477b2880ef8bff2334d6addbfbed75282e62e25e
Sara <134113685+saravorhaben@users.noreply.github.com>
2026-03-16 10:08:33 -0500

    outline for the batch funciton

--- src/batch.js
+++ src/batch.js
@@ -1,8 +1,28 @@
+import { supabase } from "./supabaseClient";
+
 // Gets the next X cars in the queue and assigns them to the correct supabase station
 async function Batch(){
     // get number of stations
+    const stations = await supabase 
+        .from("stations")
+        .select("*");
+
+    
+
     // while queue not empty
-        //  get X cars from queue
+    
+    //  get X cars from queue
+        // get queue where status is pending
+    const nextCars = await supabase
+        .from("queue")
+        .select("*")
+        .eq("status", "NA")
+        .order("created_at", ascending=true)
+        .limit(stations.length);
+    
         // assign each to a station
-        // mark each as assigned
+    nextCars.forEach(car=> {
+        // add to Cars_stations join table
+    });
+    
 }
